bbbb94f88a8c01c139b9465bc5661904eead5a64
Madzahttr <nanonetnz@gmail.com>
2019-09-14 01:18:20 +1200

    first commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <Header />
+    </div>
+</template>
+
+<script>
+import './assets/css/main.css'
+import Header from './components/layouts/Header'
+
+export default {
+    name: "app",
+    components: {
+        Header
+    }
+};
+</script>
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/leaanthony/mewn"
+	"github.com/wailsapp/wails"
+)
+
+func basic() string {
+	return "Hello World!"
+}
+
+func main() {
+
+	js := mewn.String("./frontend/dist/app.js")
+	css := mewn.String("./frontend/dist/app.css")
+
+	app := wails.CreateApp(&wails.AppConfig{
+		Width:     1024,
+		Height:    768,
+		Resizable: true,
+		Title:     "RadioControl",
+		JS:        js,
+		CSS:       css,
+		Colour:    "#131313",
+	})
+	app.Bind(basic)
+	app.Run()
+}
